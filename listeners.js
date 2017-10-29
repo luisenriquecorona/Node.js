4ee9ff29d9dd0bbd33651ef2b3c53ad987ba0060
morales <31807357+luisenriquecorona@users.noreply.github.com>
2017-10-28 23:47:34 -0500

    Delete listeners.js

--- listeners.js
+++
@@ -1,9 +0,0 @@
-request.addListener("data", function(chunk) {
-    // funcion llamada cuando un nuevo trozo (chunk) 
-    // de informacion (data) es recibido.
-});
-
-request.addListener("end", function() {
-    // funcion llamada cuando todos los trozos (chunks) 
-    // de informacion (data) han sido recibidos.
-});
